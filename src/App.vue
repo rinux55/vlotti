1c597ae7e7fda31bd3fadec8080053306c0fb24b
rinux <rinux55@gmail.com>
2022-06-05 14:28:32 +0200

    added prettier

--- src/App.vue
+++ src/App.vue
@@ -1,2 +1,6 @@
 <template>
+    <wui-button>test</wui-button>
 </template>
+<script setup lang="ts">
+import wuiButton from "./components/button/wui-button.vue"
+</script>
---
+++ src/components/button/wui-button.vue
@@ -0,0 +1,8 @@
+<template>
+    <button
+        data-cy="button"
+        class="rounded border-1 border-gray-300 bg-gray-100 p-3 hover:bg-blue-600 shadow"
+    >
+        <slot />
+    </button>
+</template>
