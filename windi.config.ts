bbcb3c627d4ddfe8e888362ff10913ccfc0e6f1b
rinux <rinux55@gmail.com>
2022-11-11 13:39:18 +0100

    updated windi shadows

--- windi.config.ts
+++ windi.config.ts
@@ -46,6 +46,18 @@ export default defineConfig({
       full: "9999px",
       large: "12px",
     },
+    boxShadow: {
+      sm: "0px 2px 3px -1px rgba(0, 0, 0, 0.1), 0px 8px 8px 5px rgba(0, 0, 0, 0.02)",
+      DEFAULT:
+        "0px 1px 3px 0px rgba(0, 0, 0, 0.15), 0px 8px 15px 5px rgba(0, 0, 0, 0.04)",
+      md: "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
+      lg: "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
+      xl: "0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)",
+      "2xl": "0 25px 50px -12px rgba(0, 0, 0, 0.25)",
+      "3xl": "0 35px 60px -15px rgba(0, 0, 0, 0.3)",
+      inner: "inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)",
+      none: "none",
+    },
   },
   safelist,
 })
